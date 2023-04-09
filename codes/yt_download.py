2cc9eb5685f2b2dd848e11af8ca66736bf56b5dc
Mauricio Alvarado <mauricio.alvarado@pucp.edu.pe>
2023-04-09 17:19:13 -0500

    rename: ruta -> path

--- codes/yt_download.py
+++ codes/yt_download.py
@@ -6,7 +6,7 @@ from pytube import YouTube
 
 # Definir 'url' de video y 'ruta' de descarga
 url = 'https://www.youtube.com/watch?v=OPf0YbXqDm0'
-ruta = './Descargas/'
+path = './Descargas/'
 
 video = YouTube(url)
-video.streams.get_highest_resolution().download(ruta)
+video.streams.get_highest_resolution().download(path)
